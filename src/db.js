cc19f8a08c4ad0a0a1351c97b098aa296ef66dba
JulioMendozaC <julio.mendozc@gmail.com>
2024-03-04 17:57:04 -0600

    variable mongo

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://mongo:3fDA6bghe5fFh4c1aA4GGb4aD16Ce5GH@viaduct.proxy.rlwy.net:11913");
+    await mongoose.connect("mongodb+srv://luckasyuki:CYjdTu4J12d541b1@cluster0.u3zed8w.mongodb.net/?retryWrites=true&w=majority");
     console.log('>>>Data Base Connect')
   } catch (error) {
     console.log(error)
