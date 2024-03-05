0e94dd9834f064e778bf70f4afe05a90cdf78c5e
JulioMendozaC <julio.mendozc@gmail.com>
2024-03-04 20:17:55 -0600

    variable port

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export const TOKEN_SECRET = 'TOKEN SECRET'
 
-export const  URL_FORNT = process.env.URL_FORNT || 'http://localhost:1420' 
+export const  URL_FORNT = process.env.URL_FORNT || 'http://localhost:1420'
+
+export const PORT =  process.env.PORT || 3000
