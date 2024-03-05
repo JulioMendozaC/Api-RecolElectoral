b578c76f19b607f0e782cc1cfb7913dd0ee9d796
JulioMendozaC <julio.mendozc@gmail.com>
2024-03-04 22:08:01 -0600

    Login error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,101 +7,107 @@ import 'dotenv/config'
 
 export const register = async (req, res) => {
 
-    const { email, password, username } = req.body
+  const { email, password, username } = req.body
 
-    const userFound = await User.findOne({username})
+  const userFound = await User.findOne({ username })
 
-    if(userFound)
+  if (userFound)
     return res.status(400).json(["El usuarios ya esa en uso"])
 
-    try {
+  try {
 
-        const hash = await bcrypt.hash(password, 10)
+    const hash = await bcrypt.hash(password, 10)
 
-        const newUser = new User({
-            username,
-            email,
-            password: hash,
-        })
+    const newUser = new User({
+      username,
+      email,
+      password: hash,
+    })
 
-        const userSave = await newUser.save()
-        const token = await CreateAccessToken({id: userSave._id});
+    const userSave = await newUser.save()
+    const token = await CreateAccessToken({ id: userSave._id });
 
 
-        res.cookie("token", token);
+    res.cookie("token", token);
 
-        res.json({
-            id: userSave.id,
-            username: userSave.username,
-            email: userSave.email
-        })
+    res.json({
+      id: userSave.id,
+      username: userSave.username,
+      email: userSave.email
+    })
 
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 
 }
 
 export const login = async (req, res) => {
 
-    const { username, password } = req.body
+  try {
+    const { username, password } = req.body;
+    const userFound = await User.findOne({ username });
 
-    try {
+    if (!userFound)
+      return res.status(400).json({
+        message: ["El usuario es incorrecto"],
+      });
 
-        const userFound = await User.findOne({username});
-        if (!userFound) return res.status(400)(json({message: "userr no found"}));
-
-        const isMatch = await bcrypt.compare(password, userFound.password);
-        if (!isMatch) res.status(400)(json({message: "invalid password"}));
-
-
-        const token = await CreateAccessToken({id: userFound._id});
-
-        res.cookie("token", token);
-        res.json({
-          id: userFound._id,
-          username: userFound.username,
-          email: userFound.email,
-        })
-
-    } catch (error) {
-        res.status(500).json( error);
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: ["La contraseña es incorrecta"],
+      });
     }
 
+    const token = await CreateAccessToken({
+      id: userFound._id,
+      username: userFound.username,
+    });
+
+ 
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 }
 
 export const logout = (req, res) => {
-    res.cookie("token", "", {
-      expires: new Date(0),
-    });
-    return res.sendStatus(200);
+  res.cookie("token", "", {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
 }
 
 export const profile = async (req, res) => {
-    const userFound = await User.findById(req.user.id);
-  
-    if (!userFound) return res.status(400).json({message: "User no found"});
-  
+  const userFound = await User.findById(req.user.id);
+
+  if (!userFound) return res.status(400).json({ message: "User no found" });
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+  });
+};
+
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+
+  jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
+    if (err) return res.status(401).json({ message: "Unauthorized" });
+
+    const userFound = await User.findById(user.id);
+    if (!userFound) return res.status(401).json({ message: "Unauthorized" });
+
     return res.json({
       id: userFound._id,
       username: userFound.username,
       email: userFound.email,
     });
-  };
-  
-  export const verifyToken = async (req, res) => {
-    const {token} = req.cookies;
-  
-    jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
-      if (err) return res.status(401).json({message: "Unauthorized"});
-  
-      const userFound = await User.findById(user.id);
-      if (!userFound) return res.status(401).json({message: "Unauthorized"});
-  
-      return res.json({
-        id: userFound._id,
-        username: userFound.username,
-        email: userFound.email,
-      });
-    });
-  };
+  });
+};
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import app from './app.js';
 import 'dotenv/config'
 
 app.listen(process.env.PORT)
-console.log("Server on port", 3000)
+console.log("Server on port", process.env.PORT)
