da1f0209d67821db4e76dac0d515ea49dcd886ed
JulioMendozaC <julio.mendozc@gmail.com>
2024-03-13 23:29:03 -0600

    Secure

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -64,7 +64,9 @@ export const login = async (req, res) => {
 
     res.cookie("token", token,
     {
-      sameSite: 'none'
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true
     });
     res.json({
       id: userFound._id,
